39d13992e2c013f48ec2ffef2a0002adaf998e78
Marcelo Nicolas Romani <nicolasromani.dev@gmail.com>
2024-08-04 12:00:08 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+
+const createWindow = () => {
+   const win = new BrowserWindow({
+      width: 300,
+      height: 300,
+      useContentSize: true,
+      resizable: false,
+      autoHideMenuBar: true,
+   })
+
+   win.loadFile(path.join('index.html'))
+   win.webContents.openDevTools({ mode: 'detach' })
+}
+
+app.whenReady().then(() => {
+   createWindow()
+})
